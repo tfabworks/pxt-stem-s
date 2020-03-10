6edb93fa351c4bb8bd56e053d70bd005fe43aaed
atsushi morita <himarayayukiotoko@gmail.com>
2020-03-10 12:11:43 +0900

    first commit

---
+++ mb.ts
@@ -0,0 +1,149 @@
+enum DarkOrBrightSpecified {
+    //% block="DARK"
+    IS_DARK,
+    //% block="BRIGHT"
+    IS_BRIGHT,
+}
+
+enum SettingHotCold {
+    //% block="HOT"
+    HOT,
+    //% block="COLD"
+    COLD,
+}
+
+enum OutputNumberFormat {
+    //% block="INTEGER"
+    INTEGER = 0,
+    //% block="FLOAT"
+    FLOAT = 1
+}
+
+//% groups='["micro:bit本体", "S-M1", "DS1", "IR2", "TP2", "KR1"]'
+//% weight=70 icon="\uf0c3" color=#ff6347 block="STEM"
+namespace stem_s {
+    let _今まで暗い: boolean = false;
+    const _暗い判定閾値: number = 20;
+    const _明るい判定閾値: number = 25;
+    const _HYSTERESIS: number = _明るい判定閾値 - _暗い判定閾値;
+
+    /**
+     * micro:bit本体の明るさセンサーが暗い場合（20未満）に真を返します。
+     */
+    //% blockId=is_dark block="is dark"
+    //% group="micro:bit本体"
+    export function isDark(): boolean {
+        return _isDark(_暗い判定閾値, _明るい判定閾値);
+
+    }
+
+    /* 明るさの平均を取る */
+    function _lightLevelSampling(): number {
+        const CYCLE_SAMPLE_NUM: number = 50
+        let accum明るさ: number = 0;
+        for (let i = 0; i < CYCLE_SAMPLE_NUM; i++) {
+            accum明るさ += input.lightLevel();
+            basic.pause(1);
+        }
+        let 明るさ = accum明るさ / CYCLE_SAMPLE_NUM;
+        return 明るさ;
+    }
+
+    /* 暗い判定本体 */
+    function _isDark(暗い判定閾値: number, 明るい判定閾値: number): boolean {
+        if ((暗い判定閾値 > 明るい判定閾値)
+            || (暗い判定閾値 < 0)
+            || (暗い判定閾値 > 255)
+            || (明るい判定閾値 < 0)
+            || (明るい判定閾値 > 255)) {
+            control.assert(false, "threshold is abnormal");
+        }
+
+        let 現在の明るさ = _lightLevelSampling();
+
+        const 暗い: boolean = true;
+        const 明るい: boolean = false;
+
+        if (_今まで暗い) { //現在まで暗い環境だったとき。明るいかを判定
+            if (現在の明るさ > 明るい判定閾値) {
+                _今まで暗い = 明るい;
+                return 明るい; //現在は明るい
+            }
+            else {
+                _今まで暗い = 暗い;
+                return 暗い; //現在は暗い
+            }
+        }
+        else { // 現在まで明るい環境だったとき。暗いかを判定
+            if (現在の明るさ < 暗い判定閾値) {
+                _今まで暗い = 暗い;
+                return 暗い; //現在は暗い
+            }
+            else {
+                _今まで暗い = 明るい;
+                return 明るい; //現在は明るい
+            }
+        }
+        control.assert(false);
+    }
+
+    /**
+     * micro:bit本体の明るさセンサーが閾値より暗い（または明るい）場合に真を返します。
+     * @param lightThreshold number of brightness-threshold, eg: 15
+     */
+    //% blockId=brightness_determination
+    //% block="Than %lightThreshold, %settingDarkOrBright"
+    //% lightThreshold.min=0 lightThreshold.max=255
+    //% group="micro:bit本体"
+    export function brightnessDetermination(lightThreshold: number, settingDarkOrBright: DarkOrBrightSpecified): boolean {
+        if (_HYSTERESIS < 0) { control.assert(false); }
+        if (lightThreshold < 0) {
+            lightThreshold = 0;
+        }
+        if (lightThreshold > 255) {
+            lightThreshold = 255;
+        }
+
+        if (settingDarkOrBright === DarkOrBrightSpecified.IS_DARK) {
+            let 暗い判定閾値: number = lightThreshold;
+            let 明るい判定閾値: number = lightThreshold + _HYSTERESIS;
+            if (明るい判定閾値 > 255) { 明るい判定閾値 = 255; }
+            return _isDark(暗い判定閾値, 明るい判定閾値);
+        }
+        else if (settingDarkOrBright === DarkOrBrightSpecified.IS_BRIGHT) {
+            let 暗い判定閾値: number = lightThreshold - _HYSTERESIS;
+            let 明るい判定閾値: number = lightThreshold;
+            if (暗い判定閾値 < 0) { 暗い判定閾値 = 0; }
+            return !_isDark(暗い判定閾値, 明るい判定閾値);
+        }
+        control.assert(false); return false;
+    }
+
+
+
+    let is_acc_first_time: boolean = true;
+    let prev_accelaration: number;
+    let acceleration: number;
+    const MOVING_THRESHOLD: number = 25;
+
+    /**
+     * micro:bit本体が揺り動かされた場合に真を返します。
+     */
+    //% blockId=is_move
+    //% block="micro:bit is moving"
+    //% group="micro:bit本体"
+    export function isMove(): boolean {
+        if (is_acc_first_time) {
+            acceleration = input.acceleration(Dimension.Strength)
+            is_acc_first_time = false;
+            return false;
+        }
+        prev_accelaration = acceleration;
+        acceleration = input.acceleration(Dimension.Strength);
+
+        if (Math.abs((acceleration - prev_accelaration)) > MOVING_THRESHOLD) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ s-m1.ts
@@ -0,0 +1,157 @@
+namespace stem_s {
+
+    /**
+     * TFW-S-M1の人感センサーが反応しているとき真を返します。
+     */
+    //% blockId=human_detection block="Is Human moving"
+    //% group="S-M1"
+    export function humanDetection(): boolean {
+        if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * micro:bit本体の明るさセンサーが暗い場合（20未満）、かつ TFW-S-M1の人感センサーが反応しているとき真を返します。
+     */
+    //% blockId=is_human_detection_and_dark
+    //% block="Is Dark and Human Moving"
+    //% group="S-M1"
+    export function isHumanDetectionAndDark(): boolean {
+        if (humanDetection() && isDark()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * TFW-S-M1のスイッチをONします。
+     */
+    //% blockId=turn_on block="Switch Turn ON"
+    //% group="S-M1"
+    export function turnON(): void {
+        if (pins.analogReadPin(AnalogPin.P1) < 25) {
+            pins.digitalWritePin(DigitalPin.P1, 1);
+        }
+        else {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        }
+    }
+
+    /**
+     * TFW-S-M1のスイッチをOFFします。
+     */
+    //% blockId=turn_off block="Switch Turn OFF"
+    //% group="S-M1"
+    export function turnOFF(): void {
+        if (pins.analogReadPin(AnalogPin.P1) < 25) {
+            pins.digitalWritePin(DigitalPin.P1, 0);
+        }
+        else {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        }
+    }
+
+    /**
+     * TFW-S-M1のスイッチの出力をコントロールします。
+     * @param duty set the duty-ratio, eg: 100
+     */
+    //% blockId=sw1_out
+    //% block="Output %duty\\%"
+    //% duty.min=0 duty.max=100
+    //% group="S-M1"
+    export function sw1_out(duty: number): void {
+        pins.analogWritePin(AnalogPin.P0, (duty / 100 * 1023));
+    }
+
+    let EN1_init_done: boolean = false;
+
+    /**
+     * TFW-S-M1で温度[℃]を測定します。
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId=get_temperature block="Temperature[degC] (S-M1) || %format"
+    //% group="S-M1"
+    export function getTemperature(format: OutputNumberFormat = OutputNumberFormat.INTEGER): number {
+        EN1_init_if_firsttime();
+        if (format === OutputNumberFormat.INTEGER) {
+            return Math.round(BME280_I2Cs.Temperature());
+        }
+        return BME280_I2Cs.Temperature();
+    }
+
+    /**
+     * TFW-S-M1で湿度[%]を測定します。
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId=get_humidity block="Humidity[\\%] || %format"
+    //% group="S-M1"
+    export function getHumidity(format: OutputNumberFormat = OutputNumberFormat.INTEGER): number {
+        EN1_init_if_firsttime();
+        if (format === OutputNumberFormat.INTEGER) {
+            return Math.round(BME280_I2Cs.Humidity());
+        }
+        return BME280_I2Cs.Humidity();
+    }
+
+    /**
+     * TFW-S-M1で気圧[hPa]を測定します。
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId=get_pressure block="Pressure[hPa] || %format"
+    //% group="S-M1"
+    export function getPressure(format: OutputNumberFormat = OutputNumberFormat.INTEGER): number {
+        EN1_init_if_firsttime();
+        if (format === OutputNumberFormat.INTEGER) {
+            return Math.round(BME280_I2Cs.Pressure());
+        }
+        return BME280_I2Cs.Pressure();
+    }
+
+    /**
+     * TFW-S-M1で基準面の気圧との差から高度差[m]を計算します。
+     * @param referencePressure 基準面の気圧[hPa], eg: 1013
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId=get_altitude block="Altitude[m] Pressure at reference level%referencePressure| || %format"
+    //% group="S-M1"
+    export function getAltitude(referencePressure: number = 1013, format: OutputNumberFormat = OutputNumberFormat.INTEGER): number {
+        EN1_init_if_firsttime();
+        if (format === OutputNumberFormat.INTEGER) {
+            return Math.round(calcHeight(referencePressure, BME280_I2Cs.Pressure(), BME280_I2Cs.Temperature()));
+        }
+        return calcHeight(referencePressure, BME280_I2Cs.Pressure(), BME280_I2Cs.Temperature());
+    }
+
+    function calcHeight(P0: number, P: number, T: number) {
+        let calcHeight_n = 0
+        let calcHeight_xa = 0
+        let calcHeight_fn = 0
+        let calcHeight_kn = 0
+        let calcHeight_a = 0
+        let calcHeight_x = 0
+        let calcHeight_Result = 0
+        calcHeight_x = P0 / P
+        calcHeight_a = 1 / 5.257
+        calcHeight_kn = calcHeight_a
+        calcHeight_fn = 1
+        calcHeight_xa = 1
+        for (let calcHeight_index = 0; calcHeight_index <= 4; calcHeight_index++) {
+            calcHeight_n = calcHeight_index + 1
+            calcHeight_xa = calcHeight_xa + calcHeight_kn * (calcHeight_x - 1) ** calcHeight_n / calcHeight_fn
+            calcHeight_kn = calcHeight_kn * (calcHeight_a - calcHeight_n)
+            calcHeight_fn = calcHeight_fn * (calcHeight_n + 1)
+        }
+        calcHeight_Result = (calcHeight_xa - 1) * (T + 273.15) / 0.0065;
+        return calcHeight_Result;
+    }
+
+    function EN1_init_if_firsttime(): void {
+        if (EN1_init_done == false) {
+            BME280_I2Cs.Init(BME280_I2C_ADDRESS.e_0x76);
+            EN1_init_done = true;
+        }
+    }
+}
