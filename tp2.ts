daaed609d44feebf29103282447470b54b1bb32e
atsushi-morita <morita@tfabworks.com>
2021-02-01 21:40:12 -0800

    add tp2 function

---
+++ tp2.ts
@@ -0,0 +1,16 @@
+namespace stem_s {
+    /**
+     * TFW-TP2の温度[℃]を返します。
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId = TP2_getTemperature
+    //% block="Temperature[degC] (TP2) || %format"
+    //% group="TP2"
+    //% weight=100
+    export function TP2_getTemperature(format: OutputNumberFormat_s = OutputNumberFormat_s.INTEGER): number {
+        if (format === OutputNumberFormat_s.INTEGER) {
+            return Math.round(DS18B20.Temperature()/100.0);
+        }
+        return DS18B20.Temperature()/100.0;
+    }
+}
